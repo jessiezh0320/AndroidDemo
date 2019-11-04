c15f53a1f9db8d1866208aa46527d1315ab19b43
Jessie <michelle3208@163.com>
2019-11-04 10:09:35 +0800

    a simple android demo

---
+++ AndroidApp1/AndroidApp1/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V7.Widget;
+
+namespace AndroidApp1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ImageView addButton;
+        ImageView searchButton;
+        EditText searchText;
+        RecyclerView myRecyclerView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            myRecyclerView = (RecyclerView)FindViewById(Resource.Id.myRecyclerView);
+        }
+    }
+}
